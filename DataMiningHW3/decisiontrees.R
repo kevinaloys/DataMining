c3d2a3df92ce45eadcf6494fc285cd662f13cf1f
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-11-18 00:07:42 -0800

    Delete.

--- DataMiningHW3/decisiontrees.R
+++
@@ -1,3 +0,0 @@
-housedata <- read.table("housetype_data.txt", header = TRUE, sep =',',na.strings=c("NA", "-", "?"))
-housedata <- data.frame(housedata)
-housedata.factor <- lapply(housedata[,1:14], as.factor())
